a09db97a6f5b5ab898a987fc1aaf6d019bda39c6
Ahurricaneeos <aurora_sala@hotmail.it>
2023-05-22 14:40:38 +0200

    Added Scaffolding

--- LaMiaPizzeria/Program.cs
+++ LaMiaPizzeria/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using la_mia_pizzeria_static.Database;
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<PizzaContext>();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<PizzaContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -20,8 +29,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseAuthentication();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Pizza}/{action=Index}/{id?}");
 
+app.MapRazorPages();
 app.Run();
